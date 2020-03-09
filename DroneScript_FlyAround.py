2a563b5a7797b38ef5b9f3542c5bfc44d3157653
LaneyDrones <61716002+LaneyDrones@users.noreply.github.com>
2020-03-09 05:13:08 -0700

    Update DroneScript_FlyAround.py

--- DroneScript_FlyAround.py
+++ DroneScript_FlyAround.py
@@ -16,6 +16,20 @@ my_drone.down(20)
 my_drone.wait(5)
 print(my_drone.get_height())
 my_drone.forward(100)
+
+"""
+#update check -- could be included too
+
+my_drone.back(100)
+
+for i in range(4):
+    my_drone.cw(90)
+    my_drone.forward(100)
+
+time.sleep(0.5)
+my_drone.up(50)
+"""
+
 my_drone.wait(10)
 print(my_drone.get_height())
 my_drone.down(20)
