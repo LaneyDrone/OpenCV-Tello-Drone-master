e3e8a98e1fdce6a10d240e5882aa5a95c68103e3
LaneyDrones <61716002+LaneyDrones@users.noreply.github.com>
2020-03-09 05:17:28 -0700

    Includes commented out height control.

--- Tello.py
+++ Tello.py
@@ -6,6 +6,8 @@ import time
 import cv2
 from easytello.stats import Stats
 import time
+#This was removed. Need to check.
+#import os
 
 class Tello:
     def __init__(self, tello_ip: str = '192.168.10.1', debug: bool = True):
@@ -35,6 +37,13 @@ class Tello:
         self.command()
         self.current_frame = None
         self.current_ret = False
+
+        """
+        #removed check
+        self.current_height = 0
+        self.new_height = 0
+        """
+
         self.file_name_prefix = "FromFlight " + str(time.time())
         self.count = 0
         self.video_thread = threading.Thread(target=self._video_thread)
@@ -218,3 +227,18 @@ class Tello:
             # waiting for good frame
             pass
         return self.current_frame
+
+    """
+    #removed check
+
+    def up_by(self, dist: int):
+        self.current_height = self.get_height()
+        self.new_height = self.current_height + dist
+        self.send_command('up', self.new_height)
+
+    def down_by(self, dist: int):
+        self.current_height = self.get_height()
+        self.new_height = self.current_height - dist
+        self.send_command('down', self.new_height)
+
+    """
