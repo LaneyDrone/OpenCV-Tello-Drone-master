2494d7e5d8bf179cd14ada0b08eb56556dd70707
LaneyDrones <61716002+LaneyDrones@users.noreply.github.com>
2020-03-17 18:23:18 -0700

    Update DroneScript_FlyAround.py

--- DroneScript_FlyAround.py
+++ DroneScript_FlyAround.py
@@ -1,5 +1,4 @@
-# an example of sending commands to the drone
-
+# An example of sending commands to drone
 import Tello
 import time
 import sys
@@ -16,23 +15,8 @@ my_drone.down(20)
 my_drone.wait(5)
 print(my_drone.get_height())
 my_drone.forward(100)
-
-"""
-#update check -- could be included too
-
-my_drone.back(100)
-
-for i in range(4):
-    my_drone.cw(90)
-    my_drone.forward(100)
-
-time.sleep(0.5)
-my_drone.up(50)
-"""
-
 my_drone.wait(10)
 print(my_drone.get_height())
 my_drone.down(20)
 my_drone.wait(5)
 my_drone.land()
-sys.exit(0)
