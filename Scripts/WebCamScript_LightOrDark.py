83971956285fd3c10fb0b61ff8e9fcd1cc3bfc1e
LaneyDrones <61716002+LaneyDrones@users.noreply.github.com>
2020-03-17 19:22:06 -0700

    Update

--- Scripts/WebCamScript_LightOrDark.py
+++ Scripts/WebCamScript_LightOrDark.py
@@ -1,14 +1,15 @@
+# Hold something white up to your webcam.  Notice the output.  Now hold up something black.
+
 import WebCam
 import ImageTools as IT
 import time
 
 my_cam = WebCam.WebCam()
-print("initialized")
 my_cam.streamon()
 
 while True:
     frame = my_cam.get_frame()
-    shade = IT.avg_color(frame, IT.GRAY)
+    shade = IT.average_gray_value(frame)
     if shade > 100:
         print("light")
     else:
