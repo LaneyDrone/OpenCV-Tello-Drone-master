0e8658af0546762498f04788b8b301c3f8d8104d
LaneyDrones <61716002+LaneyDrones@users.noreply.github.com>
2020-03-17 18:46:19 -0700

    Add files via upload

--- Scripts/DroneScript_FlyAround.py
+++ Scripts/DroneScript_FlyAround.py
@@ -1,1 +1,22 @@
+# An example of sending commands to drone
+import Tello
+import time
+import sys
 
+my_drone = Tello.Tello()
+my_drone.streamon()
+
+my_drone.takeoff()
+time.sleep(2)
+print(my_drone.get_height())
+
+# my_drone.up(75)
+my_drone.down(20)
+my_drone.wait(5)
+print(my_drone.get_height())
+my_drone.forward(100)
+my_drone.wait(10)
+print(my_drone.get_height())
+my_drone.down(20)
+my_drone.wait(5)
+my_drone.land()
---
+++ Scripts/DroneScript_LightOrDark.py
@@ -0,0 +1,18 @@
+# An example of reading and processing drone's video feed
+
+import Tello
+import ImageTools as IT
+
+my_drone = Tello.Tello()
+
+while True:
+    frame = my_drone.get_frame()
+    shade = IT.avg_color(frame, IT.GRAY)
+    if shade > 100:
+        print("light")
+    else:
+        print("dark")
+
+
+
+
